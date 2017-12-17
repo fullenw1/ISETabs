69a162da18c7343c6738be8af509dda34fd727a8
fullenw1 <luke.forums@outlook.com>
2017-12-17 20:17:49 +0100

    Added the Help section

--- ISETabs.psm1
+++ ISETabs.psm1
@@ -1,5 +1,22 @@
 function New-ISERemoteTab
 {
+    <#
+    .SYNOPSIS
+    Create a new remote ISE tab
+
+    .DESCRIPTION
+    Create a new remote ISE tab
+
+    .PARAMETER ComputerName
+    The computer to which the remote session will be created
+
+    .EXAMPLE
+    New-ISERemoteTab -ComputerName 'Server1','Server2','Server3'
+
+    .EXAMPLE
+    'Server1','Server2','Server3' | New-ISERemoteTab
+    #>
+
     [Alias('nirt')]
 
     [CmdletBinding()]
